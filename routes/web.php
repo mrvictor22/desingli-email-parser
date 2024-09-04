67a0a995799be2fe2ad9eeb0a540e0a5a49f39f3
Victor Alfonso <vc70383@hotmail.com>
2024-09-04 09:58:36 -0600

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SesEventController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/transform', [SesEventController::class, 'transform']);
