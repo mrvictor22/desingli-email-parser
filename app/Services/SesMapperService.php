d9090f727a2d4b0a253c621f1920e2ef9def093c
Victor Alfonso <vc70383@hotmail.com>
2024-09-04 01:51:58 -0600

    WIP on mapper and services

---
+++ app/Services/SesMapperService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+use AutoMapperPlus\AutoMapper;
+use AutoMapperPlus\Configuration\AutoMapperConfig;
+use App\Models\SesEvent;
+use App\DTO\TransformedEvent;
+
+class SesMapperService
+{
+
+}
