a97a79c7ff9f58f065735f314db9d20652cd2783
Victor Alfonso <vc70383@hotmail.com>
2024-09-04 01:35:22 -0600

    start commit

---
+++ app/Models/SesEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SesEvent extends Model
+{
+    use HasFactory;
+}
