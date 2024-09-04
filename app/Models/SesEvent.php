4aedd8aa78bbbceb068d9ee4b03626e5ce549bf6
Victor Alfonso <vc70383@hotmail.com>
2024-09-04 01:46:35 -0600

    WIP
    -Agregado model

--- app/Models/SesEvent.php
+++ app/Models/SesEvent.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class SesEvent extends Model
+class SesEvent
 {
-    use HasFactory;
+    public $eventVersion;
+    public $receipt;
+    public $mail;
+    public $eventSource;
+
+    public function __construct($json)
+    {
+        $this->eventVersion = $json['eventVersion'];
+        $this->receipt = $json['ses']['receipt'];
+        $this->mail = $json['ses']['mail'];
+        $this->eventSource = $json['eventSource'];
+    }
 }
+
