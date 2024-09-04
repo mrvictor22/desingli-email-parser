948d41f7e7fb5b22b61dca8cb0415ce019d62c0b
Victor Alfonso <vc70383@hotmail.com>
2024-09-04 09:58:56 -0600

    Dissable CSRF ONLY FOR TEST PURPOSES

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Services\SesMapperService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+         $this->app->singleton(SesMapperService::class, function ($app) {
+        return new SesMapperService();
+    });
     }
 
     /**
